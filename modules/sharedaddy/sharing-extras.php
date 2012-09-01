6954332978b137856d5147501e2a318f52b1af34
BarryCarlyon <BarryCarlyon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-01 14:42:23 +0000

    forgot to add the file
    git-svn-id: https://plugins.svn.wordpress.org/jetpack-extras/trunk@593399 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/sharedaddy/sharing-extras.php
@@ -0,0 +1,15 @@
+<?php
+
+add_filter('jetpack_sharing_twitter_via', 'jetpack_extras_sharing_twitter_via', 10, 2);
+add_filter('jetpack_sharing_twitter_related', 'jetpack_extras_sharing_twitter_related', 10, 2);
+
+function jetpack_extras_sharing_twitter_via($via, $post_id) {
+	$global  = get_option( 'jetpack_extras-options', array() );
+	$via = $global['twitter_via'];
+	return $via;
+}
+function jetpack_extras_sharing_twitter_related($related, $post_id) {
+	$global  = get_option( 'jetpack_extras-options', array() );
+	$related = $global['twitter_related'];
+	return $related;
+}
