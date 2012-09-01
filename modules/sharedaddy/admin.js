86e066a889748b93bc5b2567daae4e9aa462feba
BarryCarlyon <BarryCarlyon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-01 16:23:24 +0000

    better jquery
    git-svn-id: https://plugins.svn.wordpress.org/jetpack-extras/trunk@593441 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/sharedaddy/admin.js
+++ modules/sharedaddy/admin.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function() {
 	jQuery('#jetpack_extras_add_related').click(function(event) {
 		event.preventDefault();
-		jQuery('.jetpack_extras_twitter_related_input').first().clone().find('input').val('').appendTo('#jetpack_extras_twitter_related');
+		var related = jQuery('.jetpack_extras_twitter_related_input').first().clone();
+		jQuery(related).find('input').val('');
+		jQuery(related).appendTo('#jetpack_extras_twitter_related');
 	});
 });
